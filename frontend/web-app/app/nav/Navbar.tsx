8f12164aa78ed78eea4ff3dfcd6bbf866c5c6c86
Sultan Daffa Nusantara <sultan.daffa@epson.co.id>
2025-02-28 08:41:03 +0700

    End of Section 9

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Search from "./Search";
 import Logo from "./Logo";
+import LoginButton from "./LoginButton";
+import { getCurrentUser } from "../auctions/authAction";
+import UserAction from "./UserAction";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getCurrentUser();
   return (
     <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-500 shadow-md">
       <Logo />
       <Search />
-      <div>Login</div>
+      {user ? <UserAction user={user} /> : <LoginButton />}
     </header>
   );
 }
