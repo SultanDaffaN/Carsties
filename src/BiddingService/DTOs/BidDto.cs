1365fbb563c230dc466acedd2da3bf35606051a8
Sultan Daffa Nusantara <sultan.daffa@epson.co.id>
2025-03-04 14:17:44 +0700

    End of Section 13

--- src/BiddingService/DTOs/BidDto.cs
+++ src/BiddingService/DTOs/BidDto.cs
@@ -2,8 +2,9 @@ namespace BiddingService.DTOs
 {
     public class BidDto
     {
-        public string AuctionId { get; set; }
-        public string Bidder { get; set; }
+       public required string Id { get; set; }
+        public required string AuctionId { get; set; }
+        public required string Bidder { get; set; }
         public DateTime BidTime { get; set; }  = DateTime.UtcNow;
         public int Amount { get; set; }
         public string BidStatus { get; set; }
